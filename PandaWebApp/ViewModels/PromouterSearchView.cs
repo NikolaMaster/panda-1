5e2778a737c2edb32c3fa677edb9d0141b42f341
redrick-tmn <redrick.tmn@gmail.com>
2013-09-23 00:41:30 +0600

    Search improvement

--- PandaWebApp/ViewModels/PromouterSearchView.cs
+++ PandaWebApp/ViewModels/PromouterSearchView.cs
@@ -10,17 +10,20 @@ namespace PandaWebApp.ViewModels
 {
     public class PromouterSearchView
     {
-        [ValueFrom(Constants.FirstNameCode)]
-        public string FirstName { get; set; }
-        [ValueFrom(Constants.LastNameCode)]
-        public string LastName { get; set; }
-        [ValueFrom(Constants.MiddleNameCode)]
-        public string MiddleName { get; set; }
+        
         [ValueFrom(Constants.SalaryCode)]
         public string Salary { get; set; }
         [ValueFrom(Constants.CityCode)]
         public string City { get; set; }
+        [ValueFrom(Constants.GenderCode)]
+        public string Gender { get; set; }
+        [ValueFrom(Constants.AboutCode)]
+        public string About { get; set; }
 
+        public string FullName { get; set; }
+        public int Days { get; set; }
+        public int DaysOnSite { get; set; }
+        public string AvatarUrl { get; set; }
         public Checklist Checklist { get; set; }
     }
 }
